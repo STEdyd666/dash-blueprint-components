f86e49f5de57f16fb3d8a6f579d32f1372825416
Tobias Van Damme <tobias.van.damme@skf.com>
2024-08-26 14:03:39 +0200

    Removed properties that were extracted from props but not passed or used. This fixes issue where controlling the value of the control via callback wouldn't show the selected value in the component. Added disabled to the props list and no longer passed it to BPSegmentedControl as the blueprint library doesn't have a disabled property for the SegmentedControl.

--- src/lib/components/form-controls/SegmentedControl.js
+++ src/lib/components/form-controls/SegmentedControl.js
@@ -1,102 +1,112 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { SegmentedControl as BPSegmentedControl } from "@blueprintjs/core";
+import {SegmentedControl as BPSegmentedControl} from '@blueprintjs/core';
 
 
 /**
-* A SegmentedControl is a linear collection of buttons which allows a user to choose an option from multiple choices, 
-similar to a Radio group. Compared to the ButtonGroup component, SegmentedControl has affordances to signify a 
-selection UI and a reduced visual weight which is appropriate for forms.
-*/
+ * A SegmentedControl is a linear collection of buttons which allows a user to choose an option from multiple choices,
+ similar to a Radio group. Compared to the ButtonGroup component, SegmentedControl has affordances to signify a
+ selection UI and a reduced visual weight which is appropriate for forms.
+ */
 const SegmentedControl = props => {
-
     const {
-        children,
-        defaultValue,
         disabled,
-        value,
         setProps,
         ...others
     } = props;
 
-    const handleOnValueChange = (value, _) => {
-      if (!disabled) {
-          setProps({
-              value: value
-          })
-      }
-    }
+
+    const handleOnValueChange = (newValue, _) => {
+        if (!disabled) {
+            setProps({
+                value: newValue,
+            });
+        }
+    };
 
     return (
         <BPSegmentedControl
-          defaultValue={defaultValue}
-          disabled={disabled}
-          onValueChange={handleOnValueChange}
-          {...others}
+            onValueChange={handleOnValueChange}
+            {...others}
         />
-    )
-}
+    );
+};
 
 SegmentedControl.propTypes = {
     /**
      * The ID used to identify this component in Dash callbacks.
      */
     id: PropTypes.string,
-    
+
     /**
-    * A space-delimited list of class names to pass along to a child element.
-    */
+     * A space-delimited list of class names to pass along to a child element.
+     */
     className: PropTypes.string,
 
     /**
-    * Initial value. Mutually exclusive with value prop.
-    */
+     * Initial value when using component in uncontrolled mode. If value prop is
+     * also defined, this value is ignored. Setting this prop, does not initialize
+     * the value prop. Use the value prop if you need to listen or read the value
+     * of the component in a callback, i.e. use the value in a State or Input.
+     */
     defaultValue: PropTypes.string,
 
     /**
-    * Whether the control group should take up the full width of its container.
-    */
+     * If true, the option buttons are non-interactive. The value can still
+     * controllable via callback if disabled is true. Default is false.
+     */
+    disabled: PropTypes.bool,
+
+    /**
+     * Whether the control group should take up the full width of its container.
+     */
     fill: PropTypes.bool,
 
     /**
-    * Whether the control should appear as an inline element.
-    */
+     * Whether the control should appear as an inline element.
+     */
     inline: PropTypes.bool,
-    
+
     /**
-    * Visual intent to apply to the selected value.
-    */
+     * Visual intent to apply to the selected value.
+     */
     intent: PropTypes.string,
 
     /**
-    * Whether this control should use large buttons.
-    */
+     * Whether this control should use large buttons.
+     */
     large: PropTypes.bool,
 
     /**
-    * List of available options.
-    */
+     * List of available options.
+     */
     options: PropTypes.array,
 
     /**
-    * Whether this control should use small buttons.
-    */
+     * Whether this control should use small buttons.
+     */
     small: PropTypes.bool,
 
     /**
-    * CSS properties to apply to the root element.
-    */
-    style: PropTypes.object, 
+     * CSS properties to apply to the root element.
+     */
+    style: PropTypes.object,
 
     /**
-    * Selected value. Mutually exclusive with defaultValue prop.
-    */
+     * Selected value. When a value is given to this prop, the defaultValue is ignored.
+     * When using the value of this component as a state or input in a callback,
+     * use this property instead of defaultValue.
+     */
     value: PropTypes.string,
 
     /**
-    * Dash-assigned callback that gets fired when the value changes.
-    */
-    setProps: PropTypes.func
+     * Dash-assigned callback that gets fired when the value changes.
+     */
+    setProps: PropTypes.func,
+};
+
+SegmentedControl.defaultProps = {
+    disabled: false,
 };
 
 export default SegmentedControl;
